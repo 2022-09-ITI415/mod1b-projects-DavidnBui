8329049b93ee3d0f7e0be5be1531e4c163127550
DavidnBui <davidnbui@yahoo.com>
2022-09-22 01:27:09 -0400

    Script changes
    The apple tree was jittering and didn't flow correctly so I fixed that and then made it move at a faster rate and made it so that it changed directions

--- Assets/01-Apple Picker/Prefabs/AppleTree.cs
+++ Assets/01-Apple Picker/Prefabs/AppleTree.cs
@@ -29,8 +29,6 @@ public class AppleTree : MonoBehaviour
             speed = Mathf.Abs(speed);
         }   else if (pos.x > leftAndRightEdge){
                 speed = -Mathf.Abs(speed);
-        }   else if (Random.value < chanceToChangeDirection){
-                speed *= -1;
-        }
+        }   
     }
 }
